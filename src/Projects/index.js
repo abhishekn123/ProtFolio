9ac43115eca80d3eb6a0d4358d37e9a9cd631400
Abhishek <abhiabhishekn164@gmail.com>
2023-09-09 14:41:06 +0530

    Added Gemba projects

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -2,9 +2,11 @@ import { Image } from '@chakra-ui/image'
 import { Badge, Box, Heading, HStack, SimpleGrid, Text } from '@chakra-ui/layout'
 import React from 'react';
 import project from "../assets/Project-1.png"
+import digitalgemba from "../assets/DigitalGemba.png"
 
 export default function Projects() {
     return (
+        <>
         <Box w="full" marginTop="8">
             <Heading p="2" as="h5" size="md">Breacup</Heading>
             <SimpleGrid columns={{sm:1,md:1,lg:2}} spacing={10}>
@@ -23,5 +25,24 @@ export default function Projects() {
                 </Box>
             </SimpleGrid>
         </Box>
+        <Box w="full" marginTop="8">
+            <Heading p="2" as="h5" size="md">Digital Gemba</Heading>
+            <SimpleGrid columns={{sm:1,md:1,lg:2}} spacing={10}>
+                <Box  >
+                    <Image src={digitalgemba} />
+                </Box>
+                <Box >
+                    <Text>
+                    Manage your products (BOM) with heararcnial  structure. Manage BOM for multiple customers. Attach drawing to each part with easy access to team.The Product management module form the base of the application.
+                        <HStack p="2"  shouldWrapChildren={true} wrap={"wrap"} >
+                            <Badge>React JS</Badge>
+                            <Badge>Express JS</Badge>
+                            <Badge>MySql</Badge>
+                        </HStack>
+                    </Text>
+                </Box>
+            </SimpleGrid>
+        </Box>
+        </>
     )
 }
